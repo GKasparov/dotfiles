98110ec3beb8a06a93cce39cdf9cbfa0416f8ce1
GKasparov <60962839+GKasparov@users.noreply.github.com>
2021-10-19 09:21:33 +1100

    Create configuration.nix

---
+++ nixos/configuration.nix
@@ -0,0 +1,162 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Allow Unfree
+  nixpkgs.config = {
+    allowUnfree = true;
+    packageOverrides = pkgs: rec {
+      polybar = pkgs.polybar.override {
+        i3Support = true;
+};
+                                 };
+                   };
+
+  # networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Set your time zone.
+  time.timeZone = "Australia/Sydney";
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+
+  networking = {
+    networkmanager.enable = true;
+                };
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  # };
+
+  # Enable the X11 windowing system.
+  services.xserver = {
+    enable = true;
+    layout = "us";
+    videoDrivers = [ "nvidia" ];
+    desktopManager.xterm.enable = false;
+    displayManager = {
+                     lightdm.enable = true;
+                     };
+    windowManager.default = "i3";
+    windowManager.i3.package = pkgs.i3-gaps;
+    windowManager.i3.enable = true;
+
+  };
+
+  # services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable CUPS to print documents.
+  services = {
+    printing.enable = true;
+    ntp.enable = true;
+           };
+  # Enable sound.
+  sound.enable = true;
+  hardware = {
+    pulseaudio.enable = true;
+    ckb-next.enable = true;
+    opengl.enable = true;
+    opengl.driSupport32Bit = true;
+             };
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.gk = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager"]; # Enable ‘sudo’ for the user.
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    wget
+    yafetch
+    neofetch
+    pfetch
+    alacritty
+    rofi
+    polybar
+    discord
+    steam
+    flameshot
+    picom
+    teams
+    libreoffice
+    gimp
+    kdenlive
+    feh
+    lxappearance
+    pavucontrol
+    desktop-file-utils
+    firefox
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "21.05"; # Did you read the comment?
+
+  # Services
+  systemd.services = {
+    ckb-next-daemon = {
+      enable = true;
+      description = "ckb-next daemon";
+        unitConfig = {
+          Type = "simple";
+                     };
+        serviceConfig = {
+          ExecStart = "/nix/store/5rvijkhn58nhwmc6xmn1di7vbx7q03vg-system-path/bin/ckb-next-daemon";
+                        };
+        wantedBy = [ "multi-user.target" ];
+                      };
+                     };
+
+  # Fonts
+  fonts.fonts = with pkgs; [
+    nerdfonts
+  ];
+}
